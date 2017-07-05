424bf5d4fa451b69128818b99a82b766f455939c
mesargent <markesargent@gmail.com>
2017-07-04 20:18:52 -0700

    added To Do List for db demo

---
+++ ToDoList/app/src/main/java/com/sargent/mark/todolist/MainActivity.java
@@ -0,0 +1,127 @@
+package com.sargent.mark.todolist;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.sargent.mark.todolist.data.Contract;
+import com.sargent.mark.todolist.data.DBHelper;
+
+public class MainActivity extends AppCompatActivity implements AddToDoFragment.OnDialogCloseListener {
+
+    private RecyclerView rv;
+    private Button button;
+    private DBHelper helper;
+    private Cursor cursor;
+    private SQLiteDatabase db;
+    ToDoListAdapter adapter;
+    private final String TAG = "mainactivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "oncreate called in main activity");
+        button = (Button) findViewById(R.id.addToDo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getSupportFragmentManager();
+                AddToDoFragment frag = new AddToDoFragment();
+                frag.show(fm, "addtodofragment");
+            }
+        });
+        rv = (RecyclerView) findViewById(R.id.recyclerView);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (db != null) db.close();
+        if (cursor != null) cursor.close();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        helper = new DBHelper(this);
+        db = helper.getWritableDatabase();
+        cursor = getAllItems(db);
+
+        adapter = new ToDoListAdapter(cursor, new ToDoListAdapter.ItemClickListener() {
+            @Override
+            public void onItemClick(int pos) {
+                Toast.makeText(MainActivity.this, String.format("item %d clicked", pos), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        rv.setAdapter(adapter);
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                long id = (long) viewHolder.itemView.getTag();
+                Log.d(TAG, "passing id: " + id);
+                removeToDo(db, id);
+                adapter.swapCursor(getAllItems(db));
+            }
+        }).attachToRecyclerView(rv);
+    }
+
+    @Override
+    public void closeDialog(int year, int month, int day, String description) {
+        addToDo(db, description, formatDate(year, month, day));
+        cursor = getAllItems(db);
+        adapter.swapCursor(cursor);
+    }
+
+    public String formatDate(int year, int month, int day) {
+        return String.format("%4d-%2d-%2d", year, month, day);
+    }
+
+    private Cursor getAllItems(SQLiteDatabase db) {
+        boolean fool = 1 == 1;
+        return db.query(
+                Contract.TABLE_TODO.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                Contract.TABLE_TODO.COLUMN_NAME_DUE_DATE
+        );
+    }
+
+    private long addToDo(SQLiteDatabase db, String description, String duedate) {
+        ContentValues cv = new ContentValues();
+        cv.put(Contract.TABLE_TODO.COLUMN_NAME_DESCRIPTION, description);
+        cv.put(Contract.TABLE_TODO.COLUMN_NAME_DUE_DATE, duedate);
+        return db.insert(Contract.TABLE_TODO.TABLE_NAME, null, cv);
+    }
+
+    private boolean removeToDo(SQLiteDatabase db, long id) {
+        Log.d(TAG, "deleting id: " + id);
+        return db.delete(Contract.TABLE_TODO.TABLE_NAME, Contract.TABLE_TODO._ID + "=" + id, null) > 0;
+    }
+}
